a425344a27a66bb09bfc51698c217de8032d10e8
lpotas <pot.lukas@gmail.com>
2016-03-09 13:32:31 +0100

    drobne poprawki

---
+++ main.js
@@ -0,0 +1,12 @@
+/**
+ * Created by lukaszpotas on 09.03.16.
+ */
+$(document).ready(function() {
+    $('#ciastka').click(function () {
+        $('#ciastka').css({
+            margin: -100
+        })
+
+
+    });
+})
