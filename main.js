c425463c764765758a3d5bc23b889c05f8afad45
lpotas <pot.lukas@gmail.com>
2016-03-19 20:31:08 +0100

    push do developa

--- main.js
+++ main.js
@@ -1,21 +1,3 @@
-//$(function() {
-//  $('a[href*="#"]:not([href="#"])').click(function() {
-//    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-//      var target = $(this.hash);
-//      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-//      if (target.length) {
-//        $('html, body').animate({
-//          scrollTop: target.offset().top
-//        }, 1000);
-//        return false;
-//      }
-//    }
-//  });
-//});
-
-
-/* Created by lukaszpotas on 09.03.16.
- */
 function setCookieValue() {
   document.cookie = 'imie=jan';
 }
