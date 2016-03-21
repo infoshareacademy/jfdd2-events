4c8f9b8aedd586b965ca01c1b4cf333833b1a17d
BartlomiejMilewski <b.milewski762@gmail.com>
2016-03-21 17:00:23 +0100

    one change at scrolling

--- SledzaceMenu.js
+++ SledzaceMenu.js
@@ -1,23 +1,12 @@
-var przycisk1 = $('.przycisk1');
-var przycisk2 = $('.przycisk2');
-var przycisk3 = $('.przycisk3');
-var logo = $('.logo');
-$(window).scroll(function(){
-  var scroll = $(window).scrollTop();
 
-  if (scroll >=350 && scroll <=800) {
-    przycisk1.removeClass('przyciskDol').addClass('przyciskX');
-  }else {
-    przycisk1.removeClass('przyciskX').addClass('przycisk');
-  }
-  if(scroll >= 850 && scroll <1280) {
-    przycisk2.removeClass('przyciskDol').addClass('przyciskX')
-  }else {
-    przycisk2.removeClass('przyciskX').addClass('przycisk')
-  }
-  if(scroll>1250) {
-    przycisk3.removeClass('przyciskDol').addClass('przyciskX')
-  }else {
-    przycisk3.removeClass('przyciskX').addClass('przycisk')
-  }
-});
+$(window).scroll(function(){
+var scrollValue = $(window).scrollTop();
+console.log(scrollValue);
+  var funkcjeValue = $(".funkcje").offset().top;
+  if(scrollValue > 950){
+    $("#funkcje").addClass('active1');
+
+  }else
+    $("#funkcje").removeClass('active1');
+  console.log(funkcjeValue);
+})
